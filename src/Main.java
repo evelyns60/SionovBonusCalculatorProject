92a5a3550bb0620e00a8c12b95b7d2433f9c3ea5
Evelyn Sionov <evesionov@gmail.com>
2022-10-31 22:22:38 -0400

    10/31/22 home final push

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,30 @@ import java.util.Scanner;
 public class Main {
     public static void main (String[]args) {
         Scanner scan = new Scanner(System.in);
-        Calculator calc = new Calculator(10, 20);
 
         System.out.println("Welcome to my calculator! Follow the directions below.");
         System.out.println("Enter an expression with *, /, +, -, %, or ^ :");
         String expression = scan.nextLine();
 
-        int firstNum;
+        String firstNumAsString = expression.substring(0, expression.indexOf(" "));
+        String secondNumAsString = expression.substring(expression.indexOf(" ") + 3);
+        double firstNum = Double.parseDouble(firstNumAsString);
+        double secondNum = Double.parseDouble(secondNumAsString);
+        String operation = expression.substring(expression.indexOf(" ") + 1, expression.indexOf(" ") + 2);
 
-
+        Calculator calc = new Calculator(firstNum,secondNum);
+        if (operation.equals("+")) {
+            System.out.println(calc.add());
+        } else if (operation.equals("-")) {
+            System.out.println(calc.subtract());
+        } else if (operation.equals("*")) {
+            System.out.println(calc.multiply());
+        } else if (operation.equals("/")) {
+            System.out.println(calc.divide());
+        } else if (operation.equals("%")){
+            System.out.println(calc.mod());
+        } else {
+            System.out.println(calc.exponent());
+        }
     }
 }
