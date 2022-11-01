1d9422a8b7e21c590bfa91153d2e081496f92f5b
Evelyn Sionov <evesionov@gmail.com>
2022-10-31 22:24:47 -0400

    10/31/22 home final push

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,6 @@ public class Main {
     public static void main (String[]args) {
         Scanner scan = new Scanner(System.in);
 
-
         System.out.println("Welcome to my calculator! Follow the directions below.");
         System.out.println("Enter an expression with *, /, +, -, %, or ^ :");
         String expression = scan.nextLine();
