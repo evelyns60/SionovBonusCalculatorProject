76de4ce9c2cf6ab248ba0bdbb0edb619bd6518b9
Evelyn Sionov <evesionov@gmail.com>
2022-10-31 21:54:03 -0400

    10/31/22 home test push

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main (String[]args) {
+        Scanner scan = new Scanner(System.in);
+        Calculator calc = new Calculator(10, 20);
+
+        System.out.println("Welcome to my calculator! Follow the directions below.");
+        System.out.println("Enter an expression with *, /, +, -, %, or ^ :");
+        String expression = scan.nextLine();
+
+
     }
 }
